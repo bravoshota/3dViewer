f3639e465defe87de0f57fd54dced79a56a38fd6
Shota Meladze <sh.meladze@gmail.com>
2018-11-26 19:54:25 +0400

    initial

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <QtWidgets/QApplication>
+#include "mainWindow.h"
+#include "scene3d.h"
+#include "stdlib.h"
+
+// Entry point of application
+int main(int argc, char** argv)
+{
+    QApplication app(argc, argv);
+
+    // Create MainWindow object
+    MainWindow window;
+
+    // Set title and size of new window
+    window.setWindowTitle("Skeleton Viewer");
+    window.resize(800, 600);
+
+    // Show and activate window
+    window.show();
+    window.activateWindow();
+    window.raise();
+
+    // Return error if arise
+    return app.exec();
+}
